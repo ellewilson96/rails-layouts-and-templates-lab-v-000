b1d90aed8d1e3e214d62c559a5700d9383021ba6
Elle Wilson <eleanorwilsonn@gmail.com>
2018-07-17 19:42:00 +0000

    automatically backed up by learn

--- app/controllers/store_admin_controller.rb
+++ app/controllers/store_admin_controller.rb
@@ -1,3 +1,17 @@
-class StoreAdminController < ApplicationController  
-    layout 'admin'
+class StoreAdminController < ApplicationController
+  def home
+    render :layout => "home"
+  end
+
+  def orders
+    render :orders
+    render :layout => "order_administration"
+  end
+
+  def
+    def invoice
+      render :layout => false
+    end
+
+
 end
